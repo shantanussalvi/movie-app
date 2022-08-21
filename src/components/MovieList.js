16426fe9c45b63cc64d8716d8f4febd9a7b9547f
shantanussalvi <shantanussalvi@gmail.com>
2022-08-22 00:44:49 +0530

    add favourites

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -7,14 +7,15 @@ const MovieList = (props) => {
   return (
     <>
         {/* Displays movie poster using the link from OMDB api json having object name Poster */}
-        {props.movies.map((movie, index) => (
+
+        {(props.movies !== null) ? (props.movies.map((movie, index) => (
             <div className='image-container d-flex justify-content-start m-3' key={index}>
                 <img src={movie.Poster} alt={movie.Title}></img>
                 <div className='overlay d-flex align-items-center justify-content-center' onClick={() => props.handleFavouritesClick(movie)}>
                   <FavouriteComponent />
                 </div>
             </div>
-        ))}
+        ))):null}
     </>
   )
 }
